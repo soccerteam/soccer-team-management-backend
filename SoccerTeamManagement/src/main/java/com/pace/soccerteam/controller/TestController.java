dd581de63aeed7985a3018fd319e0f56df8a12a1
Sujay <sujaysail10@gmail.com>
2022-12-04 05:26:27 -0500

    basic mail api added - /sendmMail

--- SoccerTeamManagement/src/main/java/com/pace/soccerteam/controller/TestController.java
+++ SoccerTeamManagement/src/main/java/com/pace/soccerteam/controller/TestController.java
@@ -1,16 +1,22 @@
 package com.pace.soccerteam.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pace.soccerteam.email.EmailService;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/test")
 public class TestController {
 
+	@Autowired
+	private EmailService emailService;
 
 	  @GetMapping("/all")
 	  public String allAccess() {
@@ -27,6 +33,13 @@ public class TestController {
 	  @PreAuthorize("hasRole('ADMIN')")
 	  public String adminAccess() {
 	    return "Admin Board.";
+	  }
+	  
+	  @GetMapping("/sendMail")
+	  public String sendMail(@RequestParam String mail) {
+		  emailService.sendSimpleMessage(mail, "test-mail", "Hey verify code");
+		  return "mail sent";
+		  
 	  }
 
 	
